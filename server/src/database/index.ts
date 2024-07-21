9437d372201a8dd504d67ff11456a1029a94e6d8
Noww01 <hector752220@gmail.com>
2024-07-20 21:41:35 -0300

    Feat: Schema/Model created

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,7 +1,14 @@
 import { connect } from 'mongoose';
 require('dotenv').config();
 
-const db = connect(String(process.env.DATABASE_URL))
-    .catch((error) => console.error('Database connection error:', error));
+const connectDB = async () => {
+    try {
+        await connect(String(process.env.DATABASE_URL))
+            .then(() => console.log('Database connected'));
+    } catch (error) {
+        console.error('Database connection error:', error);
+    }
+}
 
-export default db;
+
+export default connectDB;
